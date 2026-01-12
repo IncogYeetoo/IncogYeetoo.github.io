6083bb719c8f144ebf5fe3091c062cf9f475b1db
IncogYeeto3 <haelyeoj.tecson.college@ustangelicum.edu.ph>
2026-01-12 21:04:02 +0800

    incognito

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -1,14 +1,17 @@
 let darkMode = true;
 
 const toggleBtn = document.getElementById("themeToggle");
+const toggleIcon = toggleBtn.querySelector("img");
 
 toggleBtn.addEventListener("click", () => {
     if (darkMode) {
         document.documentElement.style.setProperty("--bg-color", "whitesmoke");
         document.documentElement.style.setProperty("--text-color", "#18183a");
+        toggleIcon.src = "./images/DayNight/night.png"; 
     } else {
         document.documentElement.style.setProperty("--bg-color", "#18183a");
         document.documentElement.style.setProperty("--text-color", "whitesmoke");
+        toggleIcon.src = "./images/DayNight/day.png";
     }
     darkMode = !darkMode;
 });
